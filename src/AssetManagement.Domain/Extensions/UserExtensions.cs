a6fe62f941b33c264dc15cd4dd10b288f31f3a7e
Tran Xuan Thanh <thanhxuantran.dev@gmail.com>
2025-05-23 02:38:56 +0000

    Fix bug #1726: add support for fullname search
    Modify search logic to support user `fullname` search
    
    Related work items: #1726

--- src/AssetManagement.Domain/Extensions/UserExtensions.cs
+++ src/AssetManagement.Domain/Extensions/UserExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using AssetManagement.Domain.Entities;
 using AssetManagement.Domain.Enums;
 
@@ -13,8 +14,8 @@ namespace AssetManagement.Domain.Extensions
             string normalizedSearchTerm = searchTerm.Trim().ToLower();
 
             return query.Where(u =>
-                u.FirstName.Trim().ToLower().Contains(normalizedSearchTerm) ||
-                u.LastName.Trim().ToLower().Contains(normalizedSearchTerm) ||
+                (u.FirstName + " " + u.LastName).Trim().ToLower().Contains(normalizedSearchTerm) ||
+                (u.LastName + " " + u.FirstName).Trim().ToLower().Contains(normalizedSearchTerm) ||
                 u.StaffCode.Contains(normalizedSearchTerm));
         }
 
@@ -37,48 +38,47 @@ namespace AssetManagement.Domain.Extensions
             return query;
         }
 
-        public static IQueryable<User> ApplySorting(this IQueryable<User> query, IList<(string, string)> sortingCriteria)
+        public static IQueryable<User> ApplySorting(
+            this IQueryable<User> query,
+            IList<(string property, string order)> sortingCriteria)
         {
-            if (sortingCriteria.Count > 0)
+            if (sortingCriteria == null || sortingCriteria.Count == 0)
+                return query;
+
+            IOrderedQueryable<User>? ordered = null;
+
+            foreach (var (property, order) in sortingCriteria)
             {
-                IOrderedQueryable<User>? orderedQuery = null;
-                foreach (var (property, order) in sortingCriteria)
+                bool descending = order.Equals("desc", StringComparison.OrdinalIgnoreCase);
+
+                // build the key selector
+                Expression<Func<User, object>> keySelector = property.ToLower() switch
                 {
-                    if (orderedQuery == null)
-                    {
-                        orderedQuery = property.ToLower() switch
-                        {
-                            "name" => order == "desc" ? query.OrderByDescending(u => u.FirstName).ThenByDescending(u => u.LastName)
-                                : query.OrderBy(u => u.FirstName).ThenBy(u => u.LastName),
-                            "code" => order == "desc" ? query.OrderByDescending(u => u.StaffCode) : query.OrderBy(u => u.StaffCode),
-                            "username" => order == "desc" ? query.OrderByDescending(u => u.Username) : query.OrderBy(u => u.Username),
-                            "joined" => order == "desc" ? query.OrderByDescending(u => u.JoinedDate) : query.OrderBy(u => u.JoinedDate),
-                            "type" => order == "desc" ? query.OrderByDescending(u => u.Type) : query.OrderBy(u => u.Type),
-                            "created" => order == "desc" ? query.OrderByDescending(u => u.CreatedDate) : query.OrderBy(u => u.CreatedDate),
-                            "updated" => order == "desc" ? query.OrderByDescending(u => u.LastModifiedDate) : query.OrderBy(u => u.LastModifiedDate),
-                            _ => query.OrderBy(u => u.Id),
-                        };
-                    }
-                    else
-                    {
-                        orderedQuery = property.ToLower() switch
-                        {
-                            "name" => order == "desc" ? orderedQuery.ThenByDescending(u => u.FirstName).ThenByDescending(u => u.LastName)
-                                : orderedQuery.ThenBy(u => u.FirstName).ThenBy(u => u.LastName),
-                            "code" => order == "desc" ? orderedQuery.ThenByDescending(u => u.StaffCode) : orderedQuery.ThenBy(u => u.StaffCode),
-                            "username" => order == "desc" ? orderedQuery.ThenByDescending(u => u.Username) : orderedQuery.ThenBy(u => u.Username),
-                            "joined" => order == "desc" ? orderedQuery.ThenByDescending(u => u.JoinedDate) : orderedQuery.ThenBy(u => u.JoinedDate),
-                            "type" => order == "desc" ? orderedQuery.ThenByDescending(u => u.Type) : orderedQuery.ThenBy(u => u.Type),
-                            "created" => order == "desc" ? orderedQuery.ThenByDescending(u => u.CreatedDate) : orderedQuery.ThenBy(u => u.CreatedDate),
-                            "updated" => order == "desc" ? orderedQuery.ThenByDescending(u => u.LastModifiedDate) : orderedQuery.ThenBy(u => u.LastModifiedDate),
-                            _ => orderedQuery.ThenBy(u => u.Id),
-                        };
-                    }
+                    "name" => u => u.FirstName + " " + u.LastName,
+                    "code" => u => u.StaffCode,
+                    "username" => u => u.Username,
+                    "joined" => u => u.JoinedDate,
+                    "type" => u => u.Type,
+                    "created" => u => u.CreatedDate,
+                    "updated" => u => u.LastModifiedDate,
+                    _ => u => u.Id,
+                };
+
+                if (ordered == null)
+                {
+                    ordered = descending
+                        ? query.OrderByDescending(keySelector)
+                        : query.OrderBy(keySelector);
                 }
-                query = orderedQuery ?? query;
+                else
+                {
+                    ordered = descending
+                        ? ordered.ThenByDescending(keySelector)
+                        : ordered.ThenBy(keySelector);
+                }
             }
 
-            return query;
+            return ordered ?? query;
         }
     }
 }
